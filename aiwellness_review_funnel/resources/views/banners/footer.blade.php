f4eb48ae1b27cffbe3ea482b1111851c2d75484e
ariezelaya <cristianarielzelaya@hotmail.com>
2020-06-27 16:52:42 -0600

    Second push front-end

--- aiwellness_review_funnel/resources/views/banners/footer.blade.php
+++ aiwellness_review_funnel/resources/views/banners/footer.blade.php
@@ -2,6 +2,7 @@
     <div >
 {{--        <img src="src/img/log-blanco_pequeno.png" alt="nuvana logo white" style="width: 200px">--}}
         {{ HTML::image('img/logo_01.png', 'banner1', array('style' => 'width: 70px; high: 70px; margin: 0px; ')) }}
+        
 
         <p style="color:#ffffff; margin-top:20px;">Copyright © <?php echo date("Y");?> AI Wellness. All rights reserved.</p>
         <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> -->
